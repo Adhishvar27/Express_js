69eda50055f5e508e47b45ad9d97392c93987f93
Adhishvar27 <adhishvar.46@gmail.com>
2025-05-19 23:42:51 +0530

    Add files via upload

---
+++ Task_9_And_Project/data/course.js
@@ -0,0 +1,9 @@
+const courses = [
+
+{ id: 1, name: "Frontend", description: "HTML, CSS, JS, React" },
+
+{ id: 2, name: "Backend", description: "Node.js, Express, MongoDB" }
+
+];
+
+module.exports=courses;
