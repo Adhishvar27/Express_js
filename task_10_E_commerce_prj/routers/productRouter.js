f2d7c2af1ca39be477a4e4dd1642523b32dde8e6
Adhishvar27 <adhishvar.46@gmail.com>
2025-05-21 18:06:42 +0530

    Add files via upload

--- task_10_E_commerce_prj/routers/productRouter.js
+++ task_10_E_commerce_prj/routers/productRouter.js
@@ -1,33 +1,12 @@
 const express=require('express');
 const routers=express.Router();
 
-const productDetail=require('../data/ProductDetail');
+const productController=require('../controllers/productController');
 
-routers.get('/',(req,res)=>{
-    const productList=productDetail.map(list => list.name);
-    res.send(`Here is the list of products : \n${productList.join(', \n')}`);
-})
+routers.get('/',productController.getproduct);
 
-routers.get('/:productid',(req,res)=>{
-    const id=parseInt(req.params.productid);
-    const product=productDetail.find(prod => prod.id === id)
-    if(product){
-         res.send(`Here is the specified product details : ${product.name}`);
-    }
-    else{
-        res.status(404).send('Enter the valid product code');
-    }
-})
+routers.get('/:productid',productController.getproductbyid);
 
-routers.post('/',(req,res)=>{
-    const newProduct=req.body;
-    productDetail.push(newProduct);
-    if(newProduct){
-        res.send(`A new product has been added. \n id = ${newProduct.id} name = ${newProduct.name}`);
-    }
-    else{
-        res.status(404).json('Enter the value in tha body.')
-    }
-})
+routers.post('/',productController.postproduct);
 
 module.exports=routers;
--- task_10_E_commerce_prj/routers/userRouter.js
+++ task_10_E_commerce_prj/routers/userRouter.js
@@ -1,31 +1,12 @@
 const express=require('express');
 const routers=express.Router();
 
-const userDetails=require('../data/userDetail');
+const userController=require('../controllers/usersController');
 
-routers.get('/',(req,res)=>{
-    const userList=userDetails.map(user => user.name);
-    res.send(`Here is the list of user : ${userList.join(', ')}`);
-})
+routers.get('/',userController.getUser);
 
-routers.get('/:id',(req,res)=>{
-    const id=parseInt(req.params.id);
-    const userFind=userDetails.find(user => user.id === id);
-    if(userFind){
-        res.send(`Fetching the user by id : ${userFind.name}`);
-    }
-})
+routers.get('/:id',userController.getUserId);
 
-routers.post('/',(req,res)=>{
-    const newUser=req.body;
-    if(newUser){
-        userDetails.push(newUser);
-        res.send(`A new user has been added \n id = ${newUser.id} name=${newUser.name}`);
-    }
-    else{
-        res.status(404).json('enter the details');
-    }
-    
-})
+routers.post('/',userController.postUser);
 
 module.exports=routers;
