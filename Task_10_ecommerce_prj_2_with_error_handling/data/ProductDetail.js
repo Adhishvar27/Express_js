d45c1314e156cc87d8c5546899219fb2c454436f
Adhishvar27 <adhishvar.46@gmail.com>
2025-05-26 19:25:00 +0530

    Add files via upload

---
+++ Task_10_ecommerce_prj_2_with_error_handling/data/ProductDetail.js
@@ -0,0 +1,11 @@
+const product=[
+    {id:101 , name:"mouse"},
+    {id:102 , name:"keyboard"},
+    {id:103 , name:"laptop"},
+    {id:104 , name:"Head Set"},
+    {id:105 , name:"monitor"},
+    {id:106 , name:"Fan"},
+    {id:107 , name:"Key chain"}
+];
+
+module.exports=product;
---
+++ Task_10_ecommerce_prj_2_with_error_handling/data/userDetail.js
@@ -0,0 +1,9 @@
+const users=[
+    {id:1 , name:'aaa'},
+    {id:2 , name:'bbb'},
+    {id:3 , name:'ccc'},
+    {id:4 , name:'ddd'},
+    {id:5 , name:'eee'}
+];
+
+module.exports=users;
